31e3e0f803ea79edc9bdd8fee5d931187c2c5277
Matei Sebastian Alexandru <86800869+MateiSebastian9@users.noreply.github.com>
2024-10-19 03:54:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Map from './components/Map';
 import DashboardDonor from './components/DashboardDonor';
+import UpdateLocation from './components/UpdateLocation'; 
 
 function ProtectedRoute({ element: Component }) {
     const { isLoggedIn } = useAuth(); // Use useAuth to check authentication state
@@ -30,6 +31,7 @@ function App() {
                         <Route path="/register" element={<Register />} />
                         <Route path="/map" element={<ProtectedRoute element={Map} />} />
                         <Route path="/dashboard" element={<ProtectedRoute element={DashboardDonor} />} />
+                        <Route path="/updatelocation" element={<ProtectedRoute component={UpdateLocation} />} /> {/* Add the UpdateLocation route */}
                         <Route path="/editaccount" element={<ProtectedRoute element={EditAccount} />} /> {/* Add the EditAccount route */}
                         <Route path="*" element={<Login />} />
                     </Routes>
